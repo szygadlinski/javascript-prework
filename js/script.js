2f6e439971791f7f5c6017f207a2c8a947edc592
Szymon Zygadliński <szygadlinski@gmail.com>
2020-11-06 20:01:16 +0100

    Add js for computer move

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-let computerMove = 'kamień';
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to ' + randomNumber);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+	computerMove = 'kamień';
+} else if (randomNumber == 2) {
+	computerMove = 'papier';
+} else {
+	computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
