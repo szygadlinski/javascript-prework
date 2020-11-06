17cb7f4d78d35a2ba6d11546621b427df1560a83
Szymon Zygadliński <szygadlinski@gmail.com>
2020-11-06 20:26:35 +0100

    Add js for game result

--- js/script.js
+++ js/script.js
@@ -29,3 +29,25 @@ if (playerInput == '1') {
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'kamień') {
+	printMessage('Remis!');
+} else if (computerMove == 'kamień' && playerMove == 'papier') {
+	printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+	printMessage('Ja wygrywam!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+	printMessage('Ja wygrywam!');
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+	printMessage('Remis!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+	printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+	printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+	printMessage('Ja wygrywam!');
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+	printMessage('Remis!')
+} else {
+	printMessage('Następnym razem postaraj się bardziej! :)')
+}
